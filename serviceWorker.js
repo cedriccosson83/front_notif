3c39bd513fb490af1b158f6f4087c01d81048f22
Cédric Cosson <cedric.cosson@outlook.fr>
2020-07-14 11:24:17 +0200

    s

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,24 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('testMessage', {})
-})
+self.addEventListener('push', function (e) {
+    var options = {
+        body: 'Pour protéger vos données arretez de mettre le nom de votre animal de compagnie en mot de passe',
+        icon: 'images/icon.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {
+                action: 'explore', title: 'Explore this new world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action: 'close', title: 'Close',
+                icon: 'images/xmark.png'
+            },
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('Astuce RGPD', options)
+    );
+});
